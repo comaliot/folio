19c2201727b774bf6ae699573057f2f4c6e0ed30
Rokas <samuolis.rokas@gmail.com>
2022-01-03 23:35:55 +0100

    Design changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('#js-scroll'),
-  smooth: true,
-  smoothMobile: true,
-  interia: 0.75,
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector('#js-scroll'),
+//   smooth: true,
+//   smoothMobile: true,
+//   interia: 0.75,
+// });
